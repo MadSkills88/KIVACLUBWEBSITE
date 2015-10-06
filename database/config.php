38b538cb16964947ae6913d479b047bf1be4f8d1
Anthony Bao <anthonybao1999@gmail.com>
2015-10-05 22:27:07 -0700

    Revert 7bbd6c1..846c1f0
    This rolls back to commit 7bbd6c1957f4bd8e187304e30d7886a2491cd8b2.

---
+++ database/config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'adminHVWDwxd');
+define('DB_PASSWORD', 'WUNb8e18_HWU');
+define('DB_DATABASE', 'awesometoad');
+?>
---
+++ index.php
@@ -0,0 +1,307 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>The Kiva Club</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="shortcut icon" href="images/kivalogo.jpg" type="image/jpg"/>
+        <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+        <link rel="stylesheet" href="assets/css/main.css" />
+        <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+    </head>
+    <body class="homepage">
+        <div id="page-wrapper">
+
+            <!-- Header -->
+            <div id="header-wrapper">
+                <div id="header" class="container">
+
+                    <!-- Logo -->
+                    <h1 id="logo"><a href="index.php">THE KIVA CLUB</a></h1>
+                    <p>A student-led organization dedicated to fundraising</p>
+
+                    <!-- Nav -->
+                    <nav id="nav">
+                        <ul>
+                            <li><a class="icon fa-home" href="#features"><span>Introduction</span></a></li>
+                            <li><a href="#content"><span>News and Updates</span></a></li>
+                            <li>
+                                <a href="#"><span>The Team</span></a>
+                                <ul>
+                                    <li><a href="#">People</a></li>
+                                    <li><a href="#">Contributors</a></li>
+                                    <li><a href="#">Sponsors</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#footer"><span>Contact Us</span></a></li>
+                        </ul>
+                    </nav>
+
+                </div>
+            </div>
+
+            <!-- Features -->
+            <div id="features-wrapper">
+                <section id="features" class="container">
+                    <header>
+                        <h2>Our Mission In <strong>The Kiva Club</strong></h2>
+                    </header>
+                    <div class="row">
+                        <div class="4u 12u(mobile)">
+
+                            <!-- Feature -->
+                            <section>
+                                <a href="#" class="image featured"><img src="http://www.kiva.org/img/w632/1976712.jpg" alt="" /></a>
+                                <header>
+                                    <h3>About us</h3>
+                                </header>
+                                <p><strong>The Kiva Club</strong>, a student-led organization, is dedicated to fundraising and micro-finance in order to provide loans for underprivileged people around 
+                                    the world who are seeking money to jump-start their enterprises. We are a chapter of <a href="http://www.kiva.org/" target="kivalink1">KIVA</a>, the world's first online lending platform connecting online lenders to entrepreneurs </p>
+                            </section>
+                        </div>
+                        <div class="4u 12u(mobile)">
+
+                            <!-- Feature -->
+                            <section>
+                                <a href="#" class="image featured"><img src="http://www.kiva.org/img/w632/1984130.jpg" alt="" /></a>
+                                <header>
+                                    <h3>Why do we exist?</h3>
+                                </header>
+                                <p>Billions of people around the world live underprivileged lives, often suffering from poverty and oppression. <strong>The Kiva Club</strong> was founded on the principle
+                                    that all people, regardless of their origin or circumstance, should be afforded the opportunity to start their own businesses and ventures.</p>
+                            </section>
+
+                        </div>
+                        <div class="4u 12u(mobile)">
+
+                            <!-- Feature -->
+                            <section>
+                                <a href="#" class="image featured"><img src="http://www.kiva.org/img/w632/1981224.jpg" alt="" /></a>
+                                <header>
+                                    <h3>What we do</h3>
+                                </header>
+                                <p>By fundraising in our local community, we hope to be able to provide safe micro-loans to hardworking 
+                                    individuals around the world. Micro-financing is essential to cultivating new businesses. We want to foster growth and encourage entrepreneurship. With the support of our generous sponsors, we can turn dreams into reality. </p>
+                            </section>
+
+                        </div>
+                    </div>
+                    <ul class="actions">
+                        <li><a href="http://www.kiva.org/" target="kivalink1" class="button icon fa-file">Tell Me More</a></li>
+                    </ul>
+                </section>
+            </div>
+
+            <!-- Banner -->
+            <div id="banner-wrapper">
+                <div class="inner">
+                    <section id="banner" class="container">
+                        <p><i >"We Rise by Lifting Others"</i> <br>
+                        </p>
+                    </section>
+                </div>
+            </div>
+
+            <!-- Main -->
+            <div id="main-wrapper">
+                <div id="main" class="container">
+                    <div class="row">
+
+                        <!-- Content -->
+                        <div id="content" class="8u 12u(mobile)">
+
+                            <!-- Post -->
+                            <article class="box post">
+                                <header>
+                                    <h2><a href="#">A good deed <strong style="color:lightgray;">brightens</strong> a <strong>dark</strong> world...</a></h2>
+                                </header>
+                                <a href="#" class="image featured"><img src="https://c2.staticflickr.com/6/5101/5647362533_0c5c284cbd.jpg" alt="" /></a>
+                                <h3>What Has KIVA Accomplished?</h3>
+                                <div class="stats g4 a pL" style="width:400px;">
+                                    <p>
+                                        Since Kiva was founded in 2005:
+                                    </p>
+                                    <ul>
+                                        <li>
+                                            <span class="number" id="lenders">1,342,900</span> Kiva lenders
+                                        </li> <br>
+                                        <li>
+                                            <span class="number" id="amount_loans">$763,518,200</span>in loans
+                                        </li> <br>
+                                        <li>
+                                            <span class="number" id="repayment_rate">98.58%</span> repayment rate
+                                        </li> <br>
+                                    </ul>
+                                </div>
+                                <div class="stats g4 z">
+                                    <p>
+                                        We work with:
+                                    </p>
+                                    <ul>
+                                        <li>
+                                            <span class="number" id="field_partners">302</span> Field Partners
+                                        </li> <br>
+                                        <li>
+                                            <span class="number" id="volunteers">450</span> volunteers around the world
+                                        </li> <br>
+                                        <li>
+                                            <span class="number" id="amount_countries">83</span> different countries
+                                        </li> <br>
+                                    </ul>
+                                </div>
+
+
+                                <ul class="actions">
+                                    <li><a href="#" class="button icon fa-file">Continue Reading</a></li>
+                                </ul>
+                            </article>
+                        </div>
+
+                        <!-- Sidebar -->
+                        <div id="sidebar" class="4u 12u(mobile)">
+
+                            <!-- Excerpts -->
+                            <section>
+                                <ul class="divided">
+                                    <li>
+
+                                        <!-- Excerpt -->
+                                        <article class="box excerpt">
+                                            <header>
+                                                <span class="date">October 5 2015</span>
+                                                <h3><a href="#"><strong>The Kiva Club</strong> is online!</a></h3>
+                                            </header>
+                                            <p>As of October 5, 2015, <strong>The Kiva Club</strong> is now online. Our website is where we will post updates and important information. Check out the latest posts!</p>
+                                        </article>
+                                    </li>
+                                </ul>
+                            </section>
+
+                            <!-- Highlights -->
+                            <section>
+                                <ul class="divided">
+                                    <li>
+
+                                        <!-- Highlight -->
+                                        <article class="box highlight">
+                                            <header>
+                                                <h3><a href="#">Something of note</a></h3>
+                                            </header>
+                                            <a href="#" class="image left"><img src="http://l3-1.kiva.org/rgit055cdcdab764f83f723372aff987add7c8c2b673-1/img/logo/kiva_121x64.png" alt="" /></a>
+                                            <p>A Message from KIVA: We are a non-profit organization with a mission to connect people through lending to alleviate poverty.
+                                                Leveraging the internet and a worldwide network of microfinance institutions, Kiva lets individuals lend as little as $25 to help create opportunity around the world. <br> We envision a world where all people - even in the most remote areas of the globe - hold the power to create opportunity for themselves and others.
+
+                                                We believe providing safe, affordable access to capital to those in need helps people create better lives for themselves and their families. <br>
+                                                100% of every dollar you lend on Kiva goes directly towards funding loans; Kiva does not take a cut.
+                                            </p>
+                                            <ul class="actions">
+                                                <li><a href="#" class="button icon fa-file">Learn More</a></li>
+                                            </ul>
+                                        </article>
+
+                                    </li>
+                                    <li>
+                                    </li>
+                                </ul>
+                            </section>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <!-- Footer -->
+            <div id="footer-wrapper">
+                <div id="footer" class="container">
+                    <header>
+                        <h2>Questions or comments? <strong>Get in touch:</strong></h2>
+                    </header>
+                    <div class="row">
+                        <div class="6u 12u(mobile)">
+                            <section>
+                                <form method="post" action="#">
+                                    <div class="row 50%">
+                                        <div class="6u 12u(mobile)">
+                                            <input name="name" placeholder="Name" type="text" />
+                                        </div>
+                                        <div class="6u 12u(mobile)">
+                                            <input name="email" placeholder="Email" type="text" />
+                                        </div>
+                                    </div>
+                                    <div class="row 50%">
+                                        <div class="12u">
+                                            <textarea name="message" placeholder="Message"></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="row 50%">
+                                        <div class="12u">
+                                            <a href="#" class="form-button-submit button icon fa-envelope">Send Message</a>
+                                        </div>
+                                    </div>
+                                </form>
+                            </section>
+                        </div>
+                        <div class="6u 12u(mobile)">
+                            <section>
+                                <p>IMPORTANT NOTE: The contact form does is not fully functional at the moment. We apologize for the inconvenience. If you have any questions, please email:
+                                    <br>
+                                    <a style="color:green; font-weight:bold;" href="mailto:asidapara@gmail.com">asidapara@gmail.com</a>
+                                </p>
+                                <div class="row">
+                                    <div class="6u 12u(mobile)">
+                                        <ul class="icons">
+                                            <li class="icon fa-home">
+                                                1234 Somewhere Road<br />
+                                                Nashville, TN 00000<br />
+                                                USA
+                                            </li>
+                                            <li class="icon fa-phone">
+                                                (000) 000-0000
+                                            </li>
+                                            <li class="icon fa-envelope">
+                                                <a href="mailto:asidapara@gmail.com">asidapara@gmail.com</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="6u 12u(mobile)">
+                                        <ul class="icons">
+                                            <li class="icon fa-twitter">
+                                                <a href="#">@untitled-tld</a>
+                                            </li>
+                                            <li class="icon fa-instagram">
+                                                <a href="#">instagram.com/untitled-tld</a>
+                                            </li>
+                                            <li class="icon fa-dribbble">
+                                                <a href="#">dribbble.com/untitled-tld</a>
+                                            </li>
+                                            <li class="icon fa-facebook">
+                                                <a href="#">facebook.com/untitled-tld</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </section>
+                        </div>
+                    </div>
+                </div>
+                <div id="copyright" class="container">
+                    <ul class="links">
+                        <li>&copy; The KIVA Club. All rights reserved.</li><li>Design: <a>Anthony Bao and Richard Bao</a></li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+
+        <!-- Scripts -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/jquery.dropotron.min.js"></script>
+        <script src="assets/js/skel.min.js"></script>
+        <script src="assets/js/skel-viewport.min.js"></script>
+        <script src="assets/js/util.js"></script>
+        <!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+        <script src="assets/js/main.js"></script>
+
+    </body>
+</html>
